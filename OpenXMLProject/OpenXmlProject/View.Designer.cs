d4205d2867a56273669ecf074d2bf7c36935bd8e
Popov Ivan <bottob79@mail.ru>
2022-02-15 11:11:35 +0300

    Add program files

---
+++ OpenXMLProject/OpenXmlProject/View.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace OpenXmlProject
+{
+    partial class View
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.myMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.myRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.myMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // myMenuStrip
+            // 
+            this.myMenuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.myMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.myMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.myMenuStrip.Name = "myMenuStrip";
+            this.myMenuStrip.Size = new System.Drawing.Size(879, 28);
+            this.myMenuStrip.TabIndex = 0;
+            this.myMenuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // myRichTextBox
+            // 
+            this.myRichTextBox.Location = new System.Drawing.Point(0, 31);
+            this.myRichTextBox.Name = "myRichTextBox";
+            this.myRichTextBox.Size = new System.Drawing.Size(867, 429);
+            this.myRichTextBox.TabIndex = 1;
+            this.myRichTextBox.Text = "";
+            // 
+            // View
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 513);
+            this.Controls.Add(this.myRichTextBox);
+            this.Controls.Add(this.myMenuStrip);
+            this.MainMenuStrip = this.myMenuStrip;
+            this.Name = "View";
+            this.myMenuStrip.ResumeLayout(false);
+            this.myMenuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip myMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox myRichTextBox;
+    }
+}
+
---
+++ OpenXMLProject/OpenXmlProject/View.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System.Windows.Forms;
+
+namespace OpenXmlProject
+{
+    public partial class View : Form
+    {
+        public View()
+        {
+            InitializeComponent();
+            openToolStripMenuItem.Click += selectXmlFile;
+        }
+
+        private void selectXmlFile(object theSender, EventArgs theArgs)
+        {
+            myRichTextBox.Clear();
+            OpenFileDialog aFileDialog = new OpenFileDialog();
+            aFileDialog.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
+            if (aFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                using (SpreadsheetDocument aSpreadsheetDocument = SpreadsheetDocument.Open(aFileDialog.FileName, false))
+                {
+                    WorkbookPart aWorkbookPart = aSpreadsheetDocument.WorkbookPart;
+                    WorksheetPart aWorksheetPart = aWorkbookPart.WorksheetParts.First();
+                    SheetData sheetData = aWorksheetPart.Worksheet.Elements<SheetData>().First();
+                    string aText;
+                    foreach (Row aRow in sheetData.Elements<Row>())
+                    {
+                        foreach (Cell aCell in aRow.Elements<Cell>())
+                        {
+                            if (aCell.CellValue != null)
+                            {
+                                aText = aCell.CellValue.Text;
+                                myRichTextBox.Text += aText;
+                            }
+                        }
+                        myRichTextBox.Text += '\n';
+                    }
+                }
+            }
+        }
+    }
+}
